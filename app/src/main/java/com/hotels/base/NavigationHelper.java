dfc725fab344bbb9fc73eaebcc7839694f427036
Ahmed ganna <andy.ahmedg@gmail.com>
2017-04-13 13:38:20 +0200

    NavigationHelper added

---
+++ app/src/main/java/com/hotels/base/NavigationHelper.java
@@ -0,0 +1,13 @@
+package com.hotels.base;
+
+import android.app.Activity;
+
+/**
+ * Created by ahmed on 13/04/17.
+ */
+
+public class NavigationHelper {
+    public static void launchLoginScreen(Activity activity){
+        //activity.startActivity(new Intent(activity,LoginActivity.class));
+    }
+}
