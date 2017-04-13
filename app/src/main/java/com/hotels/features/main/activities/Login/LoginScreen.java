b322f929e6f174585f83adf1b03ac5edfc91c122
HazemHaroun <hazemahmed45@gmail.com>
2017-04-13 14:36:37 +0200

    SplashScreen and login

--- app/src/main/java/com/hotels/features/main/activities/Login/LoginScreen.java
+++ app/src/main/java/com/hotels/features/main/activities/Login/LoginScreen.java
@@ -2,14 +2,23 @@ package com.hotels.features.main.activities.Login;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.hotels.R;
+import com.mikepenz.materialdrawer.Drawer;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class LoginScreen extends AppCompatActivity {
 
+    Drawer drawer;
+    @BindView(R.id.login_toolbar) Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_screen_activity);
+        ButterKnife.bind(this);
+        //drawer=new Drawer();
     }
 }
