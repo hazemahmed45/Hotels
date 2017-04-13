955a8186f26335f173d5282821a819f4baffc43c
HazemHaroun <hazemahmed45@gmail.com>
2017-04-13 13:52:30 +0200

    SplashScreen and login begin

--- app/src/main/java/com/hotels/base/NavigationHelper.java
+++ app/src/main/java/com/hotels/base/NavigationHelper.java
@@ -1,6 +1,9 @@
 package com.hotels.base;
 
 import android.app.Activity;
+import android.content.Intent;
+
+import com.hotels.features.main.activities.LoginScreen;
 
 /**
  * Created by ahmed on 13/04/17.
@@ -8,6 +11,6 @@ import android.app.Activity;
 
 public class NavigationHelper {
     public static void launchLoginScreen(Activity activity){
-        //activity.startActivity(new Intent(activity,LoginActivity.class));
+        activity.startActivity(new Intent(activity,LoginScreen.class));
     }
 }
---
+++ app/src/main/java/com/hotels/features/main/activities/LoginScreen.java
@@ -0,0 +1,15 @@
+package com.hotels.features.main.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hotels.R;
+
+public class LoginScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_screen_activity);
+    }
+}
