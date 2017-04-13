6d29422ff8862fec88deb6444ccba0ba8215d92a
Ahmed ganna <andy.ahmedg@gmail.com>
2017-04-13 14:36:09 +0200

    BaseFragment added

---
+++ app/src/main/java/com/hotels/base/BaseFragment.java
@@ -0,0 +1,29 @@
+package com.hotels.base;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by ahmed on 13/04/17.
+ */
+
+public class BaseFragment extends Fragment {
+
+
+    protected void showMsg(){
+        // TODO: 13/04/17 implement SuperActivityToat library stuff
+    }
+    protected void showSuccessMsg(){
+        // TODO: 13/04/17 implement SuperActivityToat library stuff
+    }
+    protected void showErrorMsg(){
+        // TODO: 13/04/17 implement SuperActivityToat library stuff
+    }
+
+    protected void showLoading(){
+        // TODO: 13/04/17 implement Progresss bar  stuff
+    }
+
+    protected void hideLoading(){
+        // TODO: 13/04/17 implement Progress bar hiding  stuff
+    }
+}
